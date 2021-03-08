dec216e615e965449ab296e4e24d56c7a773074e
Darc Z <me@darc.pro>
2021-03-08 19:48:30 +0800

    using x86_64 crate to manage the memory

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,13 @@ use bootloader::{
     BootInfo,
     entry_point,
 };
+use x86_64::structures::paging::MapperAllSizes;
 
 entry_point!(mayo_main);
 
 fn mayo_main(boot_info: &'static BootInfo) -> ! {
 
-    use mayoos::memory::active_level_4_table;
+    use mayoos::memory;
     use x86_64::VirtAddr;
 
     println!("Welcome to Mayo OS!");
@@ -24,12 +25,23 @@ fn mayo_main(boot_info: &'static BootInfo) -> ! {
     mayoos::init();
     
     let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(physical_memory_offset) };
+    let page_table = unsafe { memory::init(physical_memory_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = page_table.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
